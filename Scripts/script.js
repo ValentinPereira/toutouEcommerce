745391381e351892fe7081dc91efe2aa90f7c85d
pereira valentin <pereira-valentin@outlook.fr>
2019-06-11 18:31:52 +0200

    toutouEcommerce

---
+++ Scripts/script.js
@@ -0,0 +1,52 @@
+﻿// Déclaration des tableaux des produits disponnibles
+var food = '[{"title":"Pedigree Sachets Fraîcheur", "img":"Image/food/food_1.jpg", "desc":"Nourriture pour Chien Adulte aux 4 Saveurs en Sauce, 84 Sachets Repas de 100g","price":"36€55","ref":"001"},' +
+    '{"title":"Eukanuba - Croquettes Premium", "img":"Image/food/food_2.jpg", "desc":"Nourriture pour Chiens Adultes Grandes Races - 100% Complète et Equilibrée - Riche en Poulet frais - Sans protéines végétales cachées, OGM, conservateurs ou arôme artificiel - 15kg","price":"63€99","ref":"002"},' +
+    '{"title":"Croquette Top Budget", "img":"Image/food/food_3.jpg", "desc":"Nourriture pour Chiens Tout âges - Saveur viande - 4kg","price":"10€00","ref":"003"},' +
+    '{"title":"Edgard & Cooper", "img":"Image/food/food_4.jpg", "desc":"Patée pour chien Barquette au Saumon et Truite pour Chien - 150g","price":"1€55","ref":"004"},' +
+    '{"title":"Lily\'s Kitchen", "img":"Image/food/food_5.jpg", "desc":"Patée pour chien - Recette Civet de Chevreuil et Faisan pour Chiens - 400g","price":"3€90","ref":"005"},' +
+    '{"title":"Royal Canin", "img":"Image/food/food_6.jpg", "desc":"Lait Baby Dog Milk pour Chiot - 2Kg","price":"55€55","ref":"006"}]';
+
+var accessory = '[{"title":"Conteneur à Croquettes", "img":"Image/accessory/accessory_1.jpg", "desc":"Curver 181204 Petlife Conteneur à Croquettes Version Chiens Vert / Blanc / Gris","price":"29€75","ref":"007"},' +
+    '{"title":"Distributeur Automatique", "img":"Image/accessory/accessory_2.jpg", "desc":"Distributeur programmable et sécurisé qui est pratique avec son récipient détachable, clipsable et lavable permettant de programmer jusqu\'à 4 repas par jour","price":"119€99","ref":"008"},' +
+    '{"title":"Gamelle chien reglable", "img":"Image/accessory/accessory_3.jpg", "desc":"Support double gamelles chien en métal laqué noir, réglable en hauteur et fourni avec 2 gamelles en inox décorées. Pour petit et moyen chien.","price":"16€00","ref":"009"},' +
+    '{"title":"Niche pour chien", "img":"Image/accessory/accessory_4.jpg", "desc":"Kerbl - Niche Overview pour Chiens","price":"250€99","ref":"010"},' +
+    '{"title":"Peluche Chewbacca", "img":"Image/accessory/accessory_5.jpg", "desc":"Star Wars - Peluche Chewbacca Star Wars pour Chien - 30cm","price":"8€90","ref":"011"},' +
+    '{"title":"Gamelle pour chien", "img":"Image/accessory/accessory_6.jpg", "desc":"Bobby - Gamelle Manege noir 10","price":"6€40","ref":"012"}]';
+
+var heal = '[{"title":"Baume protecteur coussinets", "img":"Image/heal/heal_1.jpg", "desc":"Votre chien marche pieds nus toute la journée sur des surfaces abrasives ou irritantes. Ses coussinets sont mis à rude épreuve sur les trottoirs, l\'asphalte chaude, le sel ou la neige.","price":"24€00","ref":"013"},' +
+    '{"title":"Shampoing bondi", "img":"Image/heal/heal_2.jpg", "desc":"Laissez-vous emportez par les senteurs piquantes et enivrantes du bush Australien grâce à ce shampoing haut de gamme pour votre animal.","price":"19€00","ref":"014"},' +
+    '{"title":"Shampoing réparateur", "img":"Image/heal/heal_3.jpg", "desc":"Cet après-shampoing est formulé à partir d\'ingrédients 100 % naturels obtenus à partir de plantes.","price":"24€00","ref":"015"},' +
+    '{"title":"Baume dorée ", "img":"Image/heal/heal_4.jpg", "desc":"Extravagance 1, est un soin d’exception pour le nez et les pattes de votre petit trésor. Une feuille d’or 24 carats flotte sur un lit de beurre de karité bio du Ghana propre à hydrater les truffes et les coussinets les plus exigeants.","price":"86€30","ref":"016"},' +
+    '{"title":"Spa beauty", "img":"Image/heal/heal_5.jpg", "desc":"Soin relaxant à l’eau florale de Manuka bio aux vertus apaisante, à l’huile vierge de Coco hydratante et à l’avoine calmant et hydratant.","price":"30€20","ref":"017"},' +
+    '{"title":"Spa Concentré Bien-être", "img":"Image/heal/heal_6.jpg", "desc":"Une séance de SPA digne d’un palace. Votre chien va adorer ce soin relaxant à l’eau florale de Manuka bio aux vertus apaisante, à l’huile vierge de Coco hydratante et à l’avoine calmant et hydratant.","price":"29€99","ref":"018"}]';
+// Fonction qui permet l'affichage des produits
+function addProduct(jData, attr) {
+    // La méthode JSON.parse() analyse une chaîne de caractères JSON et construit la valeur JavaScript ou l'objet décrit par cette chaîne
+    // Si le type du tableau n'est pas égal à un objet alors on parse le json sinon on récupérer le tableau
+    var arrJSON = typeof jData != 'object' ? JSON.parse(jData) : jData;
+    // Parcours le tableau afin d'afficher les éléments  du tableau en html
+    $.each(arrJSON, function (index, element) {
+        $('#' + attr).append('<div class="col-md-3 mb-3">' +
+            '<figure class="card card-product">' +
+            '<div class="img-wrap">' +
+            '<img class="img-card" src="' + element.img + '" alt="Image ' + element.title + '" title="image de produit" />' +
+            '</div >' +
+            '<figcaption class="info-wrap">' +
+            '<p class="title text-dots">' + element.title + '</p>' +
+            '<p class="desc">' + element.desc + '</p>' +
+            '<div class="action-wrap">' +
+            '<a array="' + attr + '" id="' + element.ref + '" class="btn btn-success float-right addProduct"><i class="fas fa-cart-plus text-white"></i></a>' +
+            '<div class="price-wrap h5">' +
+            '<span class="price-new">' + element.price + '</span>' +
+            '</div> ' +
+            '</div > ' +
+            '</figcaption > ' +
+            '</figure > ' +
+            '</div >');
+    });
+};
+// Appel la fonction en lui attribuant le tableau et un attribut
+addProduct(food, 'food');
+addProduct(heal, 'heal');
+addProduct(accessory, 'accessory');
+
---
+++ Scripts/script1.js
@@ -0,0 +1,154 @@
+﻿$(document).ready(function () {
+    // Fonction pour ajouter un produit dans le panier
+    $(function() {$("#popModal").modal('show'); });
+    $('.addProduct').click(function () {
+        // Option pour la notification
+        toastr.options.progressBar = true;
+        //Déclaration des variables
+        var createProduct = false;
+        // Déclaration des variables avec des valeurs précis
+        var product = $(this).attr('id');
+        var array = $(this).attr('array');
+        // On fait une condition pour savoir lequel des tableaux faut qu'il utilise
+        if (array == 'food') {
+            var arrJSON = typeof food != 'object' ? JSON.parse(food) : food;
+        } else if (array == 'heal') {
+            var arrJSON = typeof heal != 'object' ? JSON.parse(heal) : heal;
+        } else if (array == 'accessory') {
+            var arrJSON = typeof accessory != 'object' ? JSON.parse(accessory) : accessory;
+        }
+        // Parcours le tableau de la class Product afin de connaître si l'article est déjà présent dans le panier
+        $.each($('.product'), function (index, element) {
+            // Si id de la ligne du tableau est égale référence du produit
+            if (element.id == product) {
+                // Récupérer la quantité actuelle avec les enfants de l'élément
+                var quantity = element.children[3].children[1].innerHTML;
+                // Incrémentation de la variable quantity
+                quantity++;
+                // Nouvelle affichage avec la nouvelle quantite
+                element.children[3].children[1].innerHTML = quantity;
+                // On met la création du produit à vrai
+                createProduct = true
+                // Affichage de la notification de la quantite
+                toastr.success('Quantite du produit : ' + quantity);
+            }
+        });
+        // Parcours le tableau de la catégorie choisie 
+        $.each(arrJSON, function (index, element) {
+            // Si création de objet n'est pas égal vrai
+            if (createProduct !== true) {
+                // Récupération du produit choisi et ajout du produit dans le panier en html
+                if (product == element.ref) {
+                    $('#addProductTable').append('<tr class="product" id="' + element.ref + '">' +
+                        '<td>' + element.ref + '</td>' +
+                        '<td><img class="imgProduct" alt="Image ' + element.title + '" title="Image de produit" src="' + element.img + '" /></td>' +
+                        '<td>' + element.title + '</td>' +
+                        '<td> <input type="button" class="deleteQuantity" value="-" /> <span class="quantity">1</span> <input type="button" class="addQuantity" value="+" /></td>' +
+                        '<td>' + element.price + '</td>' +
+                        '<td><a href="#" class="supprProduct"><i class="fas fa-times"></i></a></td>' +
+                        '</tr>'
+                    );
+                    // Notification de succès lors de l'ajout du produit dans le panier
+                    toastr.success('Ajout du produit <i>' + element.title + '</i> dans le panier');
+                }
+            }
+        });
+        // Appel les fonctions totalPrice pour le total du prix et totalProduct pour le total des produits présent dans le panier
+        totalPrice();
+        totalProduct();
+    });
+    // Fonction de la suppression de produit dans le panier
+    $('#productCard').on('click', '.supprProduct', function () {
+        // Récupérer la balise actuelle lors du clique ensuite on récupérer son parent ainsi de suite jusqu'au tr afin de supprimer la ligne entière
+        $(this).parent().parent().remove();
+        // Condition afin de savoir s'il y a des produit dans le panier 
+        if ($('#addProductTable').children().length == 0) {
+            // Met le prix total à 0
+            $('#totalPriceCommand').text('0€');
+        } else {
+            totalPrice();
+        }
+        totalProduct();
+    });
+    // Fonction pour la décrementation de la quantité
+    $('#productCard').on('click', '.deleteQuantity', function () {
+        // Récupération de la quantité (siblings = frére)
+        var modifyQuantity = $(this).siblings('span').text();
+        // Décrémentation de la quantité
+        modifyQuantity--;
+        // Si la quantité est inférieur ou égal à 0
+        if (modifyQuantity <= 0) {
+            $(this).parent().parent().remove();
+            if ($('#addProductTable').children().length == 0) {
+                $('#totalPriceCommand').text('0€');
+            }
+            totalProduct();
+        } else {
+            $(this).siblings('span').text(modifyQuantity);
+
+        }
+        totalPrice();
+    });
+    // Fonction pour la incrémentation de la quantité
+    $('#productCard').on('click', '.addQuantity', function () {
+        var modifyQuantity = $(this).siblings('span').text();
+        modifyQuantity++;
+        $(this).siblings('span').text(modifyQuantity);
+        totalPrice();
+    });
+    // Cache les sections de base
+    $('.foodCategory').hide();
+    $('.healCategory').hide();
+    $('.accessoryCategory').hide();
+    // Affichage de la section home
+    $('#btnHome').click(function () {
+        $('.home').show(1000);
+        $('.foodCategory').hide();
+        $('.healCategory').hide();
+        $('.accessoryCategory').hide();
+    });
+    // Affichage de la section foodCategory
+    $('#btnFood').click(function () {
+        $('.home').hide();
+        $('.foodCategory').show(1000);
+        $('.healCategory').hide();
+        $('.accessoryCategory').hide();
+    });
+    // Affichage de la section healCategory
+    $('#btnHeal').click(function () {
+        $('.home').hide();
+        $('.foodCategory').hide();
+        $('.healCategory').show(1000);
+        $('.accessoryCategory').hide();
+    });
+    // Affichage de la section accessoryCategory
+    $('#btnAccessory').click(function () {
+        $('.home').hide();
+        $('.foodCategory').hide();
+        $('.healCategory').hide();
+        $('.accessoryCategory').show(1000);
+    });
+    // Fonction qui calcule le prix total
+    function totalPrice() {
+        var totalPrice = 0;
+        $.each($('.product'), function (index, element) {
+            console.log(element);
+            var quantityElement = element.children[3].children[1].innerHTML;
+            var priceElement = element.children[4].innerHTML;
+            priceElement = priceElement.split('€');
+            priceElement = priceElement[0] + '.' + priceElement[1];
+            totalPrice = totalPrice + quantityElement * priceElement;
+        });
+        totalPrice = totalPrice.toFixed(2);
+        totalPrice = totalPrice.replace('.', '€');
+        $('#totalPriceCommand').text(totalPrice);
+    };
+    // Fonction qui calcule les produits présent dans le panier
+    function totalProduct() {
+        var totalProduct = 0;
+        $.each($('.product'), function (index, element) {
+            totalProduct++;
+        });
+        $('#countProduct').text(totalProduct);
+    };
+});
